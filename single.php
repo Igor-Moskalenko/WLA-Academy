daf51e0044ae280e2e4b3118e75fc45297b0eebc
Igor-Moskalenko <166796387+Igor-Moskalenko@users.noreply.github.com>
2024-04-29 22:18:59 +0300

    homework#2
    Single: add acf link

--- single.php
+++ single.php
@@ -32,7 +32,21 @@ get_header(); ?>
 						</article>
 					<?php endwhile; ?>
 				<?php endif; ?>
-				<a href="/contact/" class="button"><?php _e('Contact', 'default'); ?></a>
+
+
+				<div class="contact__button-wrapper">
+					<?php if ($link = get_field('link')):
+						$link_url = $link['url'];
+						$link_title = $link['title'];
+						$link_target = $link['target'] ? $link['target'] : '_self';
+						?>
+						<a class='button' href="<?php echo esc_url($link_url); ?>"
+							target="<?php echo esc_attr($link_target); ?>">
+							<?php echo esc_html($link_title); ?>
+						</a>
+					<?php endif; ?>
+				</div>
+
 			</div>
 			<!-- END of post content -->
 
